9dbe2a43efa6c8fc1f531823c53be8b4640062d6
Adam Dyess <adam.dyess@canonical.com>
2023-08-08 15:36:05 -0500

    avoid jsonschema and switch to pydantic==1.* to avoid rust libraries (#20)
    * avoid jsonschema and switch to pydantic==1.* to avoid rust libraries
    
    * lint adjustments
    
    * Remove unnecessary python versions
    
    * handle integration relation in such a way to parse JSON strings from each data value
    
    * properly name 32-bit arm architectre
    
    * fix linting issue

---
+++ tests/unit/test_azure_integration.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+# Learn more about testing at: https://juju.is/docs/sdk/testing
+
+from pathlib import Path
+
+import yaml
+
+from requires_azure_integration import AzureIntegrationData
+
+
+def test_parse_relation_data():
+    d = yaml.safe_load(Path("tests/data/azure_integration_data.yaml").read_text())
+    loaded = AzureIntegrationData(**d)
+    assert loaded.aad_client == "206e5ba9-5578-4990-a74d-7f4472c675a6"
